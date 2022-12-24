ca03d4f3c91df616f9ceea262804a8bd3aa1c020
spackle0 <spackle0@users.noreply.github.com>
2022-12-23 23:54:14 -0500

    canonical urls, date urls, pagination

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,14 +1,15 @@
 """Customize Admin screens"""
+# pylint: disable=too-few-public-methods,invalid-str-returned,no-member,missing-class-docstring
 from django.contrib import admin
 from .models import Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-    list_filter = ['status', 'created', 'publish', 'author']
-    search_fields = ['title', 'body']
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
-    date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,12 +2,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset() \
-            .filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -15,17 +15,17 @@ class Post(models.Model):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
 
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique_for_date="publish")
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
-            User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
-            max_length=2, choices=Status.choices, default=Status.DRAFT
+        max_length=2, choices=Status.choices, default=Status.DRAFT
     )
 
     objects = models.Manager()  # Default manager
@@ -37,3 +37,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
